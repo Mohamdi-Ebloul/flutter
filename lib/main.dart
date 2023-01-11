c1b54a28016d99416296d9ac597a8b98442a2a87
Mohamdi-Ebloul <mohamdiahmedebloul@gmail.com>
2023-01-11 23:58:35 +0000

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sante/HomePage.dart';
+import 'package:sante/bar.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: Prfil(),
     );
   }
 }
